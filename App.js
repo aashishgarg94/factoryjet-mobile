50bc49a2ca5c22a80fbb989b5d16443bbbee06ef
aashishgarg_aakraya <aashish@aakrayaresearch.com>
2021-01-24 22:54:32 +0530

    theme

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import AppNavigator from './src/routers/AppNavigator'
+import { ThemeProvider } from 'react-native-elements'
 
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
 
+const theme = {
+  colors: {
+    primary: 'red',
+    primary_light: 'blue'
+  },
+};
+
+
 export default function App() {
   return (
     <Provider store={store}>
+      <ThemeProvider theme={theme}>
     <NavigationContainer>
       <AppNavigator/>
     </NavigationContainer>
+    </ThemeProvider>
     </Provider>
   );
 }
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -3,26 +3,20 @@ import propTypes from 'prop-types'
 import { View } from 'react-native'
 import { Text } from 'react-native-elements'
 import { connect } from 'react-redux'
+import TitleBar from '../components/TitleBar'
 
 class HomeScreen extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      itemslist: []
     }
   }
   componentDidMount() {
-    console.log("mounted")
-    this.setState({ itemslist: this.props.data.itemsList})
   }
   render() {
-    const { data: { itemsList } } = this.props
-    console.log("check")
-    console.log("itemsList", itemsList)
-    console.log("itemslist", this.state.itemslist)
-
     return (
       <View>
+        <TitleBar/>
           <Text>Home Screen</Text>
       </View>
     );
